ed2be6036e1cf5722d0a5ba26868323e84237d25
kunka squishy tentacles <marcusheadgotscarm16@gmail.com>
2022-04-09 04:26:44 +0800

    implemented dynamic grid

--- wumpus.py
+++ wumpus.py
@@ -4,14 +4,49 @@ GRID_X = 7
 GRID_Y = 6
 CONTENT_SIZE = 3
 
+cells = [["?" for i in range(CONTENT_SIZE * CONTENT_SIZE)] for j in range(GRID_X * GRID_Y)]
+
+for elem in cells[0]:
+    elem += "1"
+
+def test():
+    for i in range(len(cells)):
+        for j in range(len(cells[0])):
+            cells[i][j] = i
+    
+def test2():
+    for i in range(len(cells)):
+        for j in range(len(cells[0])):
+            cells[i][j] = "{}({})".format(i,j)
+    
 def displayGrid():
 
     grid = ""
 
     for l in range(GRID_Y):
         for j in range(CONTENT_SIZE):
+            # symbols 1-3 if any
             for i in range(GRID_X):
-                grid += ((CONTENT_SIZE * 3 * " ") + "|")
+                grid += ((" {} ").format(cells[i][0]) + (" {} ").format(cells[i][1]) + (" {} ").format(cells[i][2]) + "|")
+            grid += "\n"
+
+        for k in range(GRID_X):
+            for times in range(CONTENT_SIZE):
+                grid += "---"
+            grid += "+"
+        grid += "\n"
+
+    print(grid)
+
+def displayGridDynamic():
+
+    grid = ""
+
+    for y in range(GRID_Y):
+
+        for size in range(CONTENT_SIZE):
+            for x in range(GRID_X):
+                grid += ((" {} ").format(cells[y*GRID_Y + x + y][size * CONTENT_SIZE]) + (" {} ").format(cells[y*GRID_Y + x + y][size * CONTENT_SIZE + 1]) + (" {} ").format(cells[y*GRID_Y + x + y][size * CONTENT_SIZE + 2]) + "|")
             grid += "\n"
 
         for k in range(GRID_X):
@@ -28,5 +63,7 @@ def displayGrid():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    displayGrid()
+    #test2()
+    cells[41][4] = 'W'
+    displayGridDynamic()
 
